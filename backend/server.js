961e358309bab65edcfe383b511d00cd7617f2f1
k3nnynguy3n <114941124+k3nnynguy3n@users.noreply.github.com>
2025-02-26 15:08:44 -0800

    Add middleware, and set up API routes for data retrieval

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors'); 
+require('dotenv').config(); 
 
 const app = express(); 
 
 // Connect to MongoDB Atlas
 connectDB(); 
 
+// Middleware
+app.use(cors());            // Allow frontend to access backend API
+app.use(express.json());    // Allow JSON requests
+
+// Route for health check
 app.get('/', (req, res) => {
     res.send("MongoDB Atlas is connected!");
 });
 
+// API Test Route
+app.get('/test', async (req, res) => {
+    res.send("This is a test!")
+});
+
+// Import crime model
+const crime = require('./models/crime');
+
+// API route to retrieve all crime data
+app.get('/crime-data', async (req, res) => {
+    
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log`🚀 Server running on port ${PORT}`);
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
