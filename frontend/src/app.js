313ac3138458e3b11bf5bf7013c9b09e5d096179
k3nnynguy3n <114941124+k3nnynguy3n@users.noreply.github.com>
2025-03-02 20:43:02 -0800

    Set up frontend packages, and files

---
+++ frontend/src/app.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect } from "react"; 
+import axios from "axios"; 
+import { motion } from "framer-motion"; 
+import crimeVisualization from "./components/crimeVisualization";
+import filterControls from "./components/filterControls";
+import "./styles.css"; 
+
+const app = () => {
+    
+}
