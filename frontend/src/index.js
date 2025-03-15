6f181094d69f7bdefc6d3e175429170f818662a4
k3nnynguy3n <114941124+k3nnynguy3n@users.noreply.github.com>
2025-03-14 21:50:43 -0700

    Update file path for App.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/app";  
+import App from "./pages/App";  
 import "./styles/app.css";  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
